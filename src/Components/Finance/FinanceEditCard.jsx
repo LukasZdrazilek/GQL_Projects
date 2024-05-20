137de3a13bbbe9c8205fdef8c84117b0bcb8bdae
Lukáš Zdražílek <lukas.zdrazilek@unob.cz>
2024-05-20 11:24:21 +0200

    Vytvoření delete buttonu (nefunkčního zatím)

--- src/Components/Finance/FinanceEditCard.jsx
+++ src/Components/Finance/FinanceEditCard.jsx
@@ -4,8 +4,20 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { RawUpdateFinanceAsyncAction, ChangedUpdateFinanceAsyncAction } from '../../Queries/Finance/UpdateFinanceAsyncAction'
 import { FinanceEditType } from './FinanceEditType'
+import { DeleteButton } from '../Misc/DeleteButton'
 
 export const FinanceEditCard = ({finance}) => {
+
+    const handleDelete = async (financeId) => {               
+        try {
+            // SEM NAPSAT PAK FCI NA DELETE
+            await deleteMilestoneAsyncAction(financeId);
+            alert('Finance úspěšně smazány');
+        } catch (error) {
+            console.error('Nepovedlo se smazat finance:', error);
+        }
+    };
+
     return (
         <CardCapsule title={"Finance " + finance?.name + " - atributy"}>
             
@@ -23,6 +35,11 @@ export const FinanceEditCard = ({finance}) => {
                 <Col>
                     <FinanceEditType finance={finance} />    
                 </Col>      
+            </Row>
+            <Row>
+                <Col>
+                    <DeleteButton onClick={handleDelete}>Delete</DeleteButton>
+                </Col>
             </Row>
         </CardCapsule>
     )
--- src/Components/Milestone/MilestoneEditCard.jsx
+++ src/Components/Milestone/MilestoneEditCard.jsx
@@ -3,8 +3,20 @@ import { CardCapsule, EditableAttributeText } from '@hrbolek/uoisfrontend-shared
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { RawUpdateMilestoneAsyncAction } from '../../Queries/Milestone/UpdateMilestoneAsyncAction'
+import { DeleteButton } from '../Misc/DeleteButton'
 
 export const MilestoneEditCard = ({milestone}) => {
+
+    const handleDelete = async (milestoneId) => {               
+        try {
+            // SEM NAPSAT PAK FCI NA DELETE
+            await deleteMilestoneAsyncAction(milestoneId);
+            alert('Milník úspěšně smazán');
+        } catch (error) {
+            console.error('Nepovedlo se smazat milník:', error);
+        }
+    };
+
     return (
         <CardCapsule title={"Milestone " + milestone?.name + " - atributy"}>
             
@@ -20,6 +32,11 @@ export const MilestoneEditCard = ({milestone}) => {
                 <Col>
                     <EditableAttributeText item={milestone} attributeName="enddate" label="Datum ukonceni" asyncUpdater={RawUpdateMilestoneAsyncAction} type="datetime-local" />
                 </Col>      
+            </Row>
+            <Row>
+                <Col>
+                    <DeleteButton onClick={handleDelete}>Delete</DeleteButton>
+                </Col>
             </Row>
         </CardCapsule>
     )
