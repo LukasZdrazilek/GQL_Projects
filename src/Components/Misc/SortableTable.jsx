f8239b6852a4045b1b26ccfdb4e4edec723518cc
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 14:43:32 +0200

    comments to SortableTable.jsx

--- src/Components/Misc/SortableTable.jsx
+++ src/Components/Misc/SortableTable.jsx
@@ -2,6 +2,28 @@ import { useState } from 'react';
 import { formatDate } from "../Misc/FormatDate.jsx"; // Ensure you import the formatDate function
 import { DeleteButton } from '@hrbolek/uoisfrontend-shared/src/Components/DeleteButton.jsx'
 
+/**
+ * SortableTable Component
+ *
+ * Renders a table with sortable columns and optional row editing capabilities.
+ * table data is sorted based on the clicked column header, and
+ * rows are styled based on date comparisons (overdue = red, "active" eg current date before end of project = green
+ * inactive eg, current date otuside of bouns, white).
+ *
+ * @component
+ *
+ * @param {Object[]} columns - array of column objects
+ * @param {string} columns[].key - unique key for the column
+ * @param {string} columns[].label - column headers
+ * @param {Object[]} data - array of objects representing the table rows. The properties of 
+ *                         each object matches the `key` values in the `columns` array.
+ * @param {function} renderRow - function that takes a row object and a column key 
+ *                              and returns the JSX element to be rendered in that cell.
+ * @param {boolean} [edit=false] - if true, adds delete buttons to.
+ *
+ * @returns {JSX.Element} A table element with sortable headers and customizable row content.
+ */
+
 export const SortableTable = ({ columns, data, renderRow, edit = false }) => {
     const [sortBy, setSortBy] = useState(null);
     const [sortOrder, setSortOrder] = useState('asc');
