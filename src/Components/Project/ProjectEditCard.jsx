527369d2275378c4f933573d54e4f3eac9a044ae
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 15:05:44 +0200

    Comments for ProjectEditCard.jsx

--- src/Components/Project/ProjectEditCard.jsx
+++ src/Components/Project/ProjectEditCard.jsx
@@ -6,6 +6,28 @@ import { RawUpdateProjectAsyncAction} from '../../Queries/Project/UpdateProjectA
 import { ProjectEditType } from './ProjectEditType'
 import { ProjectEditGroup} from "./ProjectEditGroup.jsx";
 
+/**
+ * ProjectEditCard Component
+ *
+ * Provides a one place to edit all project attributes in one place
+ *
+ * @component
+ *
+ * @param {Object} project - project to be edited
+ * @param {string} project.id - ID
+ * @param {string} project.name - current name, 
+ * @param {Object} [project.type] - current type of the project 
+ * @param {string} [project.type.id] - ID of the current  type (if possible).
+ * @param {Object} [project.group] - current group working on the project (optional).
+ * @param {string} [project.group.id] - group id
+ * @param {Date} project.startdate - start date
+ * @param {Date} project.enddate - end date
+ *.
+ */
+export const ProjectEditCard = ({ project }) => {
+  // ... your component code
+};
+
 
 export const ProjectEditCard = ({project}) => {
     return (
