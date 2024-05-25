55f2e75019fb85f34be03b7e33a89b975b7b240b
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 14:30:40 +0200

    comments to onClickHandler.jsx

--- src/Components/Misc/onClickHandler.jsx
+++ src/Components/Misc/onClickHandler.jsx
@@ -1,6 +1,23 @@
 import { useDispatch } from "react-redux";
 import { CreateAsyncQueryValidator } from "@hrbolek/uoisfrontend-shared/src/Store/index.js";
 
+/**
+ * onClickHandler Function
+ *
+ * Creates an event handler function that dispatches an asynchronous action and 
+ * handles the result using a validator.
+ * designed to be used with UI elements that trigger asynchronous 
+ * refreshes page after succesful completion
+ *
+ * @function
+ *
+ * @param {Object} data - data payload passed to the assyncreator
+ * @param {Function} asyncCreator - A function that takes the data and returns action to be dispatched
+ *
+ * @returns {Function} An event handler function that can be attached to an onClick event.
+ *
+ */
+
 const validator = CreateAsyncQueryValidator({error: "Něco se nepovedlo", success: "Objekt vytvořen"})
 
 export const onClickHandler = (data, asyncCreator) => {
