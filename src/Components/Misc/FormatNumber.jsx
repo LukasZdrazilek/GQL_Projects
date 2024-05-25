873ded6009e86b706c87414b5bcf7c8168b81f96
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 14:46:42 +0200

    comments to  FormatNumber.jsx

--- src/Components/Misc/FormatNumber.jsx
+++ src/Components/Misc/FormatNumber.jsx
@@ -1,3 +1,18 @@
+/**
+ * Formats a floating-point number for display.
+ *
+ * Function takes a floating-point number and converts it to a localized string
+ * with two decimal places, 
+ * If no number is provided, it returns an empty string.
+ *
+ * @function
+ * 
+ * @param {number} [numberFloat] - The floating-point number to format,
+ * undefined, null, or not provided => empty string
+ * 
+ * @returns {string} The formatted number as a string with two decimal places and grouping separators or empty string
+ */
+
 export const formatNumber = (numberFloat) => {
     if (!numberFloat) return "";
     
