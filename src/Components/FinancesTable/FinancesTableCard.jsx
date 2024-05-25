81e4b2a419658121263e4108591b7d55f17a8cac
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 12:55:18 +0200

    comments for FinancesTableCard.jsx

--- src/Components/FinancesTable/FinancesTableCard.jsx
+++ src/Components/FinancesTable/FinancesTableCard.jsx
@@ -3,6 +3,23 @@ import { formatNumber } from '../Misc/FormatNumber.jsx'
 import { FinanceLink, FinanceCreateLink} from '../Finance/FinanceLink.jsx'
 import { SortableTable } from '../Misc/SortableTable.jsx'
 
+/**
+ * FinancesTableCard Component
+ *
+ * Displays a sortable table of finances associated with the given project.
+ * Sorting works on each collumn.
+ * @component
+ *
+ * @param {Object} project - project object from @query containing connected finances
+ * @param {Object[]} project.finances - An array of finance objects 
+ * @param {string} project.finances[].name - name
+ * @param {float} project.finances[].amount - amount in czk
+ * @param {string} project.finances[].financeType[].name - finance type
+ *
+ * @returns {JSX.Element} component containing a SortableTable for displaying the finances.
+ *
+ */
+
 export const FinancesTableCard = ({project}) => {
     const columns = [
         { key: 'name', label: 'Finance' },
