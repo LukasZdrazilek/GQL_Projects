23eb6acef49eaa5a0abeec1d35f25c282a1a2000
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 13:51:34 +0200

    comments to MilestoneMediumCard.jsx

--- src/Components/Milestone/MilestoneMediumCard.jsx
+++ src/Components/Milestone/MilestoneMediumCard.jsx
@@ -6,6 +6,24 @@ import { formatDate } from '../Misc/FormatDate'
 import { MilestoneLink } from '../Milestone/MilestoneLink'
 import { ProjectLink } from '../Project/ProjectLink'
 
+/**
+ * MilestoneMediumCard Component
+ *
+ * Displays a  card with attributes of milestone
+ * 
+ * @component
+ *
+ * @param {Object} milestone - milestone object from @query
+ * @param {string} milestone.name - name
+ * @param {Object} milestone.project - parent project to which the milestone belongs
+ * @param {Date} milestone.startdate - start date
+ * @param {Date} milestone.enddate - end date
+ * @param {Object[]} milestone.previous - proceeding milestones as list
+ * @param {Object[]} milestone.nexts - following milestones as list
+ * 
+ * @returns {JSX.Element} The rendered MilestoneMediumCard component.
+ */
+
 export const MilestoneMediumCard = ({milestone}) => {
     return (
         <CardCapsule title={
