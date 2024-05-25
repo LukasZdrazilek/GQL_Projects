d275348856ad67db0b2b778ba274a41babb02019
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 14:52:37 +0200

    Comments for  FormatDate.jsx

--- src/Components/Misc/FormatDate.jsx
+++ src/Components/Misc/FormatDate.jsx
@@ -1,3 +1,19 @@
+/**
+ * Formats a date string for display.
+ *
+ * This function takes a date string in ISO 8601 format (e.g., '2024-05-25'),
+ * Converts it to a localized Czech format: "DD. MM. YYYY". 
+ * If invalid or empty date string is provided, it returns an empty string.
+ *
+ * @function
+ * 
+ * @param {string} [dateString] - The date string in ISO 8601 format to be formatted 
+ *                                (e.g., '2024-05-25').
+ * 
+ * @returns {string} The formatted date string in the format "DD. MM. YYYY"
+ *                                or an empty string if not valid
+ */
+
 export const formatDate = (dateString) => {
     if (!dateString) return ""; // If dateString is null or undefined, return empty string
     
