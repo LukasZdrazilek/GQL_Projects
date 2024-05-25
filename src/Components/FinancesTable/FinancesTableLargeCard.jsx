4ca9986655842e3556db777c58d7fcb05284d2d6
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 13:09:15 +0200

    cooments for FinancesTableLargeCard.jsx

--- src/Components/FinancesTable/FinancesTableLargeCard.jsx
+++ src/Components/FinancesTable/FinancesTableLargeCard.jsx
@@ -3,6 +3,22 @@ import {ProjectLink} from "../Project/ProjectLink.jsx";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+/**
+ * FinancesTableLargeCard Component
+ *
+ * Provides a layout structure for displaying project-related finance information 
+ * (passed via `children`) within a `CardCapsule`. 
+ * Header shows which project are the finances tied to
+ *
+ * @component
+ *
+ * @param {Object} project - The project object containing relevant information.
+ * @param {ReactNode} children - The content to be displayed within the card => tables, cards
+ *
+ * @returns {JSX.Element} A CardCapsule component containing a row with a single column 
+ *                       displaying the provided children.
+ */
+
 export const FinancesTableLargeCard = ({project, children}) => {
     return (
         <CardCapsule title={<div>
