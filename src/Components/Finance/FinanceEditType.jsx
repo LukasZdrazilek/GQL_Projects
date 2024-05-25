4ef9f3552247ed18d713e20cf756780229a059e4
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 11:03:50 +0200

    cooments to FinanceEditType.jsx

--- src/Components/Finance/FinanceEditType.jsx
+++ src/Components/Finance/FinanceEditType.jsx
@@ -4,6 +4,25 @@ import { FetchFinanceTypesAsyncAction } from '../../Queries/Finance/FetchFinance
 import { useFreshItem, CreateAsyncQueryValidator, useDispatch  } from '@hrbolek/uoisfrontend-shared/src';
 import { useState } from 'react';
 
+/**
+ * FinanceEditType Component
+ *
+ * Provides a web component with expendable dropdown to modify the finance type attribute.
+ * Editable attribte This component fetches available finance types, populates the dropdown,
+ * and handles updating the finance type on the server using asynchronous actions.
+ *
+ * @component
+ *
+ * @param {Object} finance - The finance object to be edited.
+ * @param {string} finance.id - The ID of the finance object.
+ * @param {Object} finance.financeType - The current finance type object (optional).
+ * @param {string} finance.financeType.id - The ID of the current finance type (optional).
+ *
+ * @returns {JSX.Element} The rendered FinanceEditType component 
+ * 
+ */
+
+
 const id = "43dd2ff1-5c17-42a5-ba36-8b30e2a243bb"
 export const FinanceEditType = ({finance}) => {
     const [financetypes, financetypesPromise] = useFreshItem({id}, FetchFinanceTypesAsyncAction)
