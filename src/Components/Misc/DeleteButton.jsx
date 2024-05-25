0938ff7ae50aedf348172270235784306119643a
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 15:00:54 +0200

    comments to DeleteButton.jsx

--- src/Components/Misc/DeleteButton.jsx
+++ src/Components/Misc/DeleteButton.jsx
@@ -1,5 +1,26 @@
 import React, { useState, useCallback } from 'react';
 
+/**
+ * DeleteButton Component
+ *
+ * A button that implements a double-confirmation mechanism before triggering the
+ * provided `onClick` action. Failsafe against accidental deletion
+ *
+ * @component
+ *
+ * @param {ReactNode} children - The text or content to display within the button
+ * @param {function} onClick - The function to be called when the delete action is confirmed
+ *
+ * @returns {JSX.Element} A button element that implements double confirmation.
+ *
+ * @behavior
+ * 1. Initial State:  Displays a single yellow/warning button with the `children` text, for example delete
+ * 2. First Click:  Replaces the button with two buttons: yellow/warning button to cancel and a 
+ *                  red/danger button to confirm onClick event to occur
+ * 3.1 Second Click (Cancel):  Returns to the initial state with single button
+ * 3.2 Second Click (Confirm): Executes the `onClick` function
+ */
+
 export const DeleteButton = ({children, onClick}) => 
     {
         // vnitrni stavova promenna definujici. zda je cervene tlacitko zobrazene nebo neni
