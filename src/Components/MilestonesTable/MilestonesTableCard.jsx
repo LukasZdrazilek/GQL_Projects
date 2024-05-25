bdc7aa10dac3e5e45ca74b1d6dd6a27e89a14643
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-25 14:15:43 +0200

    Comment to MilestonesTableCard.jsx

--- src/Components/MilestonesTable/MilestonesTableCard.jsx
+++ src/Components/MilestonesTable/MilestonesTableCard.jsx
@@ -3,6 +3,23 @@ import { formatDate } from "../Misc/FormatDate.jsx";
 import { CardCapsule } from "@hrbolek/uoisfrontend-shared/src";
 import { SortableTable } from "../Misc/SortableTable.jsx";
 
+/**
+ * MilestonesTableEditCard Component
+ *
+ * Displays a sortable and editable table of milestones associated with a project.
+ * Allows for creating, editing, and sorting milestones.
+ *
+ * @component
+ *
+ * @param {Object} project - The project object from @query
+ * @param {Object[]} project.milestones - list of milestones
+ * @param {string} project.milestones[].name - their name
+ * @param {Date} project.milestones[].startdate - start date
+ * @param {Date} project.milestones[].enddate - end date
+ *
+ * @returns {JSX.Element} A CardCapsule component containing a SortableTable along with a CreateButton
+ */
+
 export const MilestonesTableCard = ({ project }) => {
     const columns = [
         { key: 'name', label: 'Milníky' },
