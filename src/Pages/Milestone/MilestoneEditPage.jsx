ca59f1ca616328be719bfa9b47788c675cff132b
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 13:43:12 +0200

    comments for  MilestoneEditPage.jsx

--- src/Pages/Milestone/MilestoneEditPage.jsx
+++ src/Pages/Milestone/MilestoneEditPage.jsx
@@ -6,6 +6,20 @@ import { FetchMilestoneByIdAsyncAction } from "../../Queries/Milestone/FetchMile
 import { MilestoneLargeCard } from "../../Components/Milestone/MilestoneLargeCard.jsx"
 import { MilestoneEditCard } from "../../Components/Milestone/MilestoneEditCard.jsx"
 
+/**
+ * MilestoneEditPage Component
+ *
+ * This component is responsible for:
+ * - Retrieving milestone data using the `FetchFinanceByIdAsyncAction` based on the milestone ID extracted from the URL.
+ * - Showing basic information via MilestoneLargeCard
+ * - A loading message while the milestone data is being fetched.
+ * - A success or error message upon completion of the data fetching operation.
+ * - `MilestoneEditCard` is embedded within the `MilestoneLargeCard` to allow users to edit the attributes
+ *
+ * @component
+ *
+ */
+
 const validator = CreateAsyncQueryValidator({error: "Nepovedlo se načíst milestone", success: "Načtení milestone se povedlo"})
 export const MilestoneEditPage = ()  => {
     const {id} = useParams()
