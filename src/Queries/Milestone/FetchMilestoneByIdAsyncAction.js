f76becefb6c87d1044facabf767c844e251f2161
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 14:44:50 +0200

    comments for FetchMilestoneByIdAsyncAction.js

--- src/Queries/Milestone/FetchMilestoneByIdAsyncAction.js
+++ src/Queries/Milestone/FetchMilestoneByIdAsyncAction.js
@@ -1,5 +1,20 @@
 import { CreateAsyncActionFromQuery } from "@hrbolek/uoisfrontend-shared/src"
 
+/**
+ * FetchMilestoneByIdAsyncAction
+ *
+ * asynchronous action creator function to fetch a milestone by its ID.
+ * query to retrieve detailed information about a specific milestone,
+ * including its associated project, previous milestones, and next milestones.
+ *
+ * @function FetchMilestoneByIdAsyncAction
+ *
+ * @param {Object} params - parameters object 
+ * @param {string} params.id - The UUID of the milestone to fetch.
+ *
+ * @returns {Function} action that dispatches the GraphQL query and handles success or error responses.
+ */
+
 const query = `query ($id: UUID!) 
 {
     result: milestoneById(id: $id) 
