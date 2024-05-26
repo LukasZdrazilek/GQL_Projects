1e41b9b64f226f808e69408f43b9f205434f2aff
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 18:46:50 +0200

    comments for UpdateFinanceAsyncAction.js

--- src/Queries/Finance/UpdateFinanceAsyncAction.js
+++ src/Queries/Finance/UpdateFinanceAsyncAction.js
@@ -1,4 +1,50 @@
 import { CreateAsyncActionFromMutation } from "@hrbolek/uoisfrontend-shared/src"
+
+/**
+ * RawUpdateFinanceAsyncAction
+ *
+ * asynchronous action creator function for updating the finance object
+ * intended for direct use with FinanceEditCard
+ *
+ * Th the input data must include:
+ *   - finance ID 
+ *   - finance's last change timestam
+ *   - attributes to update:
+ *
+ * @function RawUpdateFinanceAsyncAction
+ *
+ * @param {Object} data - input data .
+ * @param {string} data.id - UUID of the finance
+ * @param {string} data.lastchange - DateTime string of last change
+ * @param {string} [data.name] -  new name 
+ * @param {number} [data.amount] - new amount
+ *
+ * @returns {Function}  action that dispatches the query
+ */
+
+/**
+ * ChangedUpdateFinanceAsyncAction
+ *
+ * asynchronous action creator function for updating the finance object
+ * intended for direct use with FinanceEditCard, 
+ * includes data conversion to correctly update finance.amount 
+ *
+ * Th the input data must include:
+ *   - finance ID 
+ *   - finance's last change timestam
+ *   - attributes to update:
+ *
+ * @function RawUpdateFinanceAsyncAction
+ *
+ * @param {Object} data - input data .
+ * @param {string} data.id - UUID of the finance
+ * @param {string} data.lastchange - DateTime string of last change
+ * @param {string} [data.name] -  new name 
+ * @param {number} [data.amount] - new amount
+ *
+ * @returns {Function}  action that dispatches the query
+ */
+
 const mutation =
 `
 mutation($id: UUID!, $lastchange: DateTime!, $name: String, $amount: Float)
