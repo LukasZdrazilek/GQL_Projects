0486fa35f139f6de2c0c4d9d2a8474772fca854c
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 19:36:57 +0200

    comments for FetchFinanceByIdAsyncAction.js

--- src/Queries/Finance/FetchFinanceByIdAsyncAction.js
+++ src/Queries/Finance/FetchFinanceByIdAsyncAction.js
@@ -1,5 +1,17 @@
 import { CreateAsyncActionFromQuery } from "@hrbolek/uoisfrontend-shared/src"
 
+/**
+ * FetchFinanceByIdAsyncAction
+ *
+ *  asynchronous action function to fetch a finance by its ID.
+ * @function FetchFinanceByIdAsyncAction
+ *
+ * @param {Object} params - object with data
+ * @param {string} params.id - uuid to be fetched
+ *
+ * @returns {Function} action that dispatches the query and handles success or error response
+*/
+
 const query = `query ($id: UUID!) 
 {
     result: financeById(id: $id) 
