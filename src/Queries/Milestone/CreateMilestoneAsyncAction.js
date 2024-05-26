c25eb4c29a3d663552440311d592db8fb9f17286
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 14:42:28 +0200

    comments for CreateMilestoneAsyncAction.js

--- src/Queries/Milestone/CreateMilestoneAsyncAction.js
+++ src/Queries/Milestone/CreateMilestoneAsyncAction.js
@@ -1,4 +1,26 @@
 import { CreateAsyncActionFromMutation } from "@hrbolek/uoisfrontend-shared/src"
+
+/**
+ * CreateMilestoneAsyncAction
+ *
+ * asynchronous action creator function for creating a new milestone.
+ *
+ * It takes an object containing `project_id` to which milestone shall belong 
+ * and `name` of new milestone as input data.
+ *
+ * The query (`milestoneInsert`) creates a new milestone associated with the
+ * specified project and returns the ID, message (`msg`), and details of the created milestone.
+ *
+ * @function CreateMilestoneAsyncAction
+ *
+ * @param {Object} data - data object containing:
+ * @param {string} data.project_id -  UUID of the project
+ * @param {string} data.name - name for new milestone
+ *
+ * @returns {Function} asynchronous action that dispatches the GraphQL mutation
+ *                     and handles success or error responses.
+ */
+
 const mutation =
     `
 mutation($project_id: UUID!, $name: String!)
