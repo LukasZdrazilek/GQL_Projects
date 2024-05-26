55a16e2fee408185d3462564672f0b31e19e6960
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 13:32:53 +0200

    comments for  FinanceEditPage.jsx

--- src/Pages/Finance/FinanceEditPage.jsx
+++ src/Pages/Finance/FinanceEditPage.jsx
@@ -5,6 +5,20 @@ import { FetchFinanceByIdAsyncAction } from "../../Queries/Finance/FetchFinanceB
 import { FinanceLargeCard } from "../../Components/Finance/FinanceLargeCard.jsx"
 import { FinanceEditCard } from "../../Components/Finance/FinanceEditCard.jsx"
 
+/**
+ * FinanceEditPage Component
+ *
+ * This component is responsible for:
+ * - Retrieving finance data using the `FetchFinanceByIdAsyncAction` based on the finance ID extracted from the URL.
+ * - Showing basic information via FinanceLargeCard
+ * - A loading message while the finance data is being fetched.
+ * - A success or error message upon completion of the data fetching operation.
+ * - `FinanceEditCard` is embedded within the `FinanceLargeCard` to allow users to edit the attributes
+ *
+ * @component
+ *
+ */
+
 const validator = CreateAsyncQueryValidator({error: "Nepovedlo se načíst finance", success: "Načtení financí se povedlo"})
 export const FinanceEditPage = ()  => {
     const {id} = useParams()
