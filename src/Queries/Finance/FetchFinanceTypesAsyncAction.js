76ab4ef2f82cd3f137f7850cda23ff92718cbaf9
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 19:19:44 +0200

    comments for FetchFinanceTypesAsyncAction.js

--- src/Queries/Finance/FetchFinanceTypesAsyncAction.js
+++ src/Queries/Finance/FetchFinanceTypesAsyncAction.js
@@ -1,5 +1,17 @@
 import { CreateAsyncActionFromQuery } from "@hrbolek/uoisfrontend-shared/src"
 
+/**
+ * FetchFinanceTypesAsyncAction
+ *
+ * asynchronous action creator function to fetch a list of finance types for the drop down component
+ *
+ * query fetches up to 100 finance types
+ * @function FetchFinanceTypesAsyncAction
+ *
+ * @returns {Function} Aaction that dispatches the GraphQL query and handles 
+ *                     success or error responses
+ */
+
 const query = `{
     result: financeTypePage(limit: 100)
     {
