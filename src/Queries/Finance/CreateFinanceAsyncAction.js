2a143709cd1ae126f5f82fedfd20c0590d192550
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 19:50:44 +0200

    comments to CreateFinanceAsyncAction.js

--- src/Queries/Finance/CreateFinanceAsyncAction.js
+++ src/Queries/Finance/CreateFinanceAsyncAction.js
@@ -1,4 +1,24 @@
 import { CreateAsyncActionFromMutation } from "@hrbolek/uoisfrontend-shared/src"
+
+/**
+ * CreateFinanceAsyncAction
+ *
+ * asynchronous action creator function for creating a new finance object
+ *
+ * `financeInsert` creates a new finance associated with the
+ * specified project and finance type, and returns the details of new object as message
+ *
+ * @function CreateFinanceAsyncAction
+ *
+ * @param {Object} data -  data object 
+ * @param {string} data.project_id - UUID of the project
+ * @param {string} data.name - name of the new finance object
+ * @param {string} data.financeType_id - UUID of the finance type for the new finance
+ *
+ * @returns {Function} A Redux-Thunk asynchronous action that dispatches the GraphQL mutation
+ *                     and handles success or error responses.
+ */
+
 const mutation =
     `
 mutation($project_id: UUID!, $name: String!, $financeType_id: UUID!) {
