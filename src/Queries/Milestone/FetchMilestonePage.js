785519509aca8374817926d2fcf9e62d4c9b77fb
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-26 14:47:33 +0200

    comments for FetchMilestonePage.js

--- src/Queries/Milestone/FetchMilestonePage.js
+++ src/Queries/Milestone/FetchMilestonePage.js
@@ -1,5 +1,18 @@
 import { CreateAsyncActionFromQuery } from "@hrbolek/uoisfrontend-shared/src"
 
+/**
+ * FetchMilestonePage
+ *
+ * asynchronous action creator function to fetch the list of milestones.
+ * query to retrieve a page of milestone IDs and names
+ *
+ * @function FetchMilestonePage
+ *
+ * @returns {Function} action that dispatches the GraphQL query and handles 
+ *                     success or error responses.
+ *
+ */
+
 const query = `{
     milestonePage {
       id
