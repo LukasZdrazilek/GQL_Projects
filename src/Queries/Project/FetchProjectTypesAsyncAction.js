5340eb09510df097c1617ae85a0d75df539bf499
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-27 08:00:47 +0200

    comments for  FetchProjectTypesAsyncAction.js

--- src/Queries/Project/FetchProjectTypesAsyncAction.js
+++ src/Queries/Project/FetchProjectTypesAsyncAction.js
@@ -1,5 +1,21 @@
   import { CreateAsyncActionFromQuery } from "@hrbolek/uoisfrontend-shared/src"
 
+/**
+ * FetchProjectTypesAsyncAction
+ *
+ * asynchronous action function to fetch a list of project types
+ *
+ * This action dispatches query (`projectTypePage`) to retrieve a  list of
+ * project types.
+ *
+ * @function FetchProjectTypesAsyncAction
+ *
+ * @returns {Function} action that dispatches the query and handles 
+ *                     success or error responses. The action payload will contain the 
+ *                     fetched project types in the `data.result` property upon success.
+
+ */
+
 const query = `{
     result: projectTypePage(limit: 100)
     {
