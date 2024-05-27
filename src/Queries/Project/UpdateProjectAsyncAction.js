52824eca4805ea3c14a3ab523c94cbb5e12d28a5
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-27 08:22:12 +0200

    comments for  UpdateProjectAsyncAction.js

--- src/Queries/Project/UpdateProjectAsyncAction.js
+++ src/Queries/Project/UpdateProjectAsyncAction.js
@@ -1,4 +1,26 @@
 import { CreateAsyncActionFromMutation } from "@hrbolek/uoisfrontend-shared/src"
+
+/**
+ * RawUpdateProjectAsyncAction
+ *
+ * asynchronous action function for updating a project's details.
+ *
+ * This action is intended for direct use with editable components
+ *
+ * @function RawUpdateProjectAsyncAction
+ *
+ * @param {Object} data - input data for the mutation
+ * @param {string} data.id - UUID of the project to update
+ * @param {string} data.lastchange - DateTime string of last change
+ * @param {string} [data.name] -  name 
+ * @param {string} [data.startdate] - new start date 
+ * @param {string} [data.enddate] -  new end date 
+ * @param {string} [data.projectType_id] -  new project type 
+ * @param {string} [data.group_id] - Thew project group ID 
+ *
+ * @returns {Function} A Redux-Thunk action that dispatches the GraphQL mutation and handles success or error responses.
+ */
+
 const mutation =
 `
 mutation($id: UUID!, $lastchange: DateTime!, $name: String, $startdate: DateTime, $enddate: DateTime, $projectType_id: UUID, $group_id: UUID)
