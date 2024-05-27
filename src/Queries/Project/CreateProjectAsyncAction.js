8406585958a71c1a83646811e5edf60fd55b9f59
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-27 07:43:45 +0200

    comments for CreateProjectAsyncAction.js

--- src/Queries/Project/CreateProjectAsyncAction.js
+++ src/Queries/Project/CreateProjectAsyncAction.js
@@ -1,4 +1,24 @@
 import { CreateAsyncActionFromMutation } from "@hrbolek/uoisfrontend-shared/src"
+
+/**
+ * CreateProjectAsyncAction
+ *
+ *  asynchronous action function for creating a new project.
+ *
+ * `projectInsert` creates a new project associated with the
+ * specified project type and returns the result message
+ *
+ * @function CreateProjectAsyncAction
+ *
+ * @param {Object} data - data object
+ * @param {string} data.projectType_id - UUID of the project type
+ * @param {string} data.name - name of the new project
+ *
+ * @returns {Function} asynchronous action that dispatches the query
+ *                     and handles success or error responses.
+
+ */
+
 const mutation =
     `
 mutation($projectType_id: UUID!, $name: String!) {
