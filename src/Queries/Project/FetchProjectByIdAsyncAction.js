4a424944f7579d530f2c8f7b1732566bd2d92fbd
Memokre <147373468+Memokre@users.noreply.github.com>
2024-05-27 07:50:24 +0200

    combat comments for  FetchProjectByIdAsyncAction.js

--- src/Queries/Project/FetchProjectByIdAsyncAction.js
+++ src/Queries/Project/FetchProjectByIdAsyncAction.js
@@ -1,5 +1,22 @@
 import { CreateAsyncActionFromQuery } from "@hrbolek/uoisfrontend-shared/src"
 
+/**
+ * FetchProjectByIdAsyncAction
+ *
+ * asynchronous action creator function to fetch a project's details by its ID
+ *
+ * This action dispatches a GraphQL query (`projectById`) to retrieve comprehensive information
+ * about a specific project
+ *
+ * @function FetchProjectByIdAsyncAction
+ *
+ * @param {Object} params - data.object 
+ * @param {string} params.id - UUID of the project 
+ *
+ * @returns {Function} action that dispatches query and handles success or error responses
+ *                     action will contain the fetched project data in the `data.result` 
+ */
+
 const query = `query ($id: UUID!) {
     result: projectById(id: $id) {
         __typename
